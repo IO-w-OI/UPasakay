5ce85d4a3829b85a741d50e2b1cf03bd0431bdac
OwO <unknowncryptooll@gmail.com>
2026-02-18 17:41:29 +0800

    Update User model to use password_hash column and override getAuthPassword for Fortify compatibility.

--- UPasakay/app/Models/User.php
+++ UPasakay/app/Models/User.php
@@ -19,9 +19,8 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
         'email',
-        'password',
+        'password_hash',
     ];
 
     /**
@@ -30,7 +29,7 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $hidden = [
-        'password',
+        'password_hash',
         'two_factor_secret',
         'two_factor_recovery_codes',
         'remember_token',
