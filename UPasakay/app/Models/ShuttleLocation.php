afe2dbac1d4e8a966d1fee5666acdfa91c210bfb
OwO <unknowncryptooll@gmail.com>
2026-02-18 17:40:55 +0800

    Add ShuttleLocation model
    Add ShuttleLocation model with fillable fields and belongsTo Shuttle relationship.

--- UPasakay/app/Models/ShuttleLocation.php
+++ UPasakay/app/Models/ShuttleLocation.php
@@ -1,10 +1,13 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class ShuttleLocation extends Model
 {
-    //
+    protected $fillable = ['shuttle_id', 'latitude', 'longitude', 'speed_kmh', 'recorded_at'];
+
+    public function shuttle()
+    {
+        return $this->belongsTo(Shuttle::class);
+    }
 }
