6a6eb6740af035c9f073a1d1f1e7b2fd2b689132
OwO <unknowncryptooll@gmail.com>
2026-02-18 17:40:38 +0800

    Add Shuttle model
    Add Shuttle model with fillable fields and hasMany ShuttleLocation relationship.

--- UPasakay/app/Models/Shuttle.php
+++ UPasakay/app/Models/Shuttle.php
@@ -1,10 +1,13 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class Shuttle extends Model
 {
-    //
+    protected $fillable = ['plate_number', 'capacity', 'is_active'];
+
+    public function locations()
+    {
+        return $this->hasMany(ShuttleLocation::class);
+    }
 }
