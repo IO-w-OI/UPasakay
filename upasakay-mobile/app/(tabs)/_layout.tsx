01b86a55f131f67ed7c8cb4c72e11df939ed6f83
Ben <benedictjohndelacruz@gmail.com>
2026-04-21 01:25:56 +0800

    Update _layout.tsx

--- upasakay-mobile/app/(tabs)/_layout.tsx
+++ upasakay-mobile/app/(tabs)/_layout.tsx
@@ -1,10 +1,5 @@
 import { Stack } from 'expo-router';
 
 export default function TabLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="Drivers" />
-      <Stack.Screen name="Users" />
-    </Stack>
-  );
+  return (<Stack />);
 }
